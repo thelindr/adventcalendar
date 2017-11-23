ba652190dc4c16dafac897aab99a109d85818d7f
Fredrik <osterlund.f@gmail.com>
2017-11-23 21:46:19 +0100

    check day -- if condition is working

--- code/script.js
+++ code/script.js
@@ -1,3 +1,7 @@
-if (true) {
+const day01 = new Date(2017,10,23)
+
+const currentTime = new Date()
+
+if (day01.getFullYear() === currentTime.getFullYear() && day01.getMonth() === currentTime.getMonth() && day01.getDate() === currentTime.getDate()) {
   document.getElementById("window-day-01").classList.toggle("window-is-open")
 }
