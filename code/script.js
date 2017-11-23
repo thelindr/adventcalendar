f8db361542bcee37aab064c1d017141715361ee9
Fredrik <osterlund.f@gmail.com>
2017-11-23 21:48:22 +0100

    update: new variable 'dayTEST'

--- code/script.js
+++ code/script.js
@@ -1,7 +1,7 @@
-const day01 = new Date(2017,10,23)
+const dayTEST = new Date(2017,10,23)
 
 const currentTime = new Date()
 
-if (day01.getFullYear() === currentTime.getFullYear() && day01.getMonth() === currentTime.getMonth() && day01.getDate() === currentTime.getDate()) {
+if (dayTEST.getFullYear() === currentTime.getFullYear() && dayTEST.getMonth() === currentTime.getMonth() && dayTEST.getDate() === currentTime.getDate()) {
   document.getElementById("window-day-01").classList.toggle("window-is-open")
 }
