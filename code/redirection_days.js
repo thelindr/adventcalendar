fce395994b76140caa0816e805d362182895c809
Fredrik <osterlund.f@gmail.com>
2017-11-26 21:32:24 +0100

    JS: added comments to redirection script

--- code/redirection_days.js
+++ code/redirection_days.js
@@ -1,19 +1,16 @@
 // FUNCTION: CHECK IF USER SHALL BE REDIRECTED
-// const redirectionCheck = (day) => {
-//   if (checkCurrentTimeAndDay(day) == 0) {
-//     window.location.href = "../index.html"
-//     }
-// }
-
 const redirectionCheck = () => {
+  // GET 'DD' IN /days/DD.html FROM THE URL OF THE CURRENT PAGE
   const indexOfFirstCharInDate = window.location.href.lastIndexOf("/") + 1
   const indexOfSecondCharInDate = window.location.href.lastIndexOf("/") + 2
   const firstCharInDate = window.location.href.charAt(indexOfFirstCharInDate)
   const secondCharInDate = window.location.href.charAt(indexOfSecondCharInDate)
   const dateInURL = `${firstCharInDate}${secondCharInDate}`
+  // IF CURRENT DATE < 2017-12-DD (2017-11-DD), THEN REDIRECT USER TO INDEX PAGE
   if (checkCurrentTimeAndDay(new Date(2017,11,dateInURL)) == 0) {
     window.location.href = "../index.html"
     }
 }
 
+// INVOKE FUNCTION
 redirectionCheck()
