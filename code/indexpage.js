f983eff890153913cc731b5d269bee9adffdf5b7
Fredrik <osterlund.f@gmail.com>
2017-11-29 22:28:57 +0100

    JS: cleanup, added more comments + improved support for test

--- code/indexpage.js
+++ code/indexpage.js
@@ -1,40 +1,24 @@
-// CHECK ALL DAYS---WILL ONLY WORK WHEN CURRENT TIME IS 1--31 DEC
-// days.forEach((day, index) => {
-//   const actualDay = index + 1
-//   const dayString = (actualDay < 10 ? "0" + actualDay : actualDay)
-//   const windowElement = document.getElementById(`window-day-${dayString}`)
-//   if (checkCurrentTimeAndDay(day) == 2) {
-//     windowElement.classList.add("window-is-open")
-//     windowElement.onclick = () => {
-//       window.location.href = `./days/${dayString}.html`
-//     }
-//   } else if (checkCurrentTimeAndDay(day) == 1) {
-//       windowElement.classList.add("window-is-openable")
-//       windowElement.onclick = () => {
-//         windowElement.classList.remove("window-is-openable")
-//         windowElement.classList.add("window-is-open")
-//         windowElement.onclick = () => {
-//           window.location.href = `./days/${dayString}.html`
-//         }
-//       }
-//     }
-// })
-
-// USE FOR TEST: APPLIED TO WINDOW FOR DAY 1
-if (checkCurrentTimeAndDay(days[24]) == 2) {
-  const windowElement = document.getElementById("window-day-01")
-  windowElement.classList.add("window-is-open")
-  windowElement.onclick = () => {
-    window.location.href = "./days/01.html"
+// FUNCTION: SET A WINDOW TO BE OPEN AND ADD ITS LINK USING 'onclick'
+const setWindowOpen = (openThisWindow, dayString) => {
+  openThisWindow.classList.add("window-is-open")
+  openThisWindow.onclick = () => {
+    window.location.href = `./days/${dayString}.html`
   }
-} else if (checkCurrentTimeAndDay(days[24]) == 1) {
-    const windowElement = document.getElementById("window-day-01")
-    windowElement.classList.toggle("window-is-openable")
-    windowElement.onclick = () => {
-      windowElement.classList.remove("window-is-openable")
-      windowElement.classList.add("window-is-open")
-      windowElement.onclick = () => {
-        window.location.href = "./days/01.html"
+}
+
+// ***START*** ITERATE OVER AN ARRAY OF DAYS TO CHECK IF THE WINDOW FOR A DAY SHALL BE OPEN OR OPENABLE
+// NOTE: THIS WILL ONLY WORK WHEN CURRENT TIME IS 1--31 DEC 2017. AFTER 31 DEC 2017, ALL WINDOWS WILL REMAIN CLOSED.
+days.forEach((day, index) => { // FOR EACH DAY IN THE ARRAY (SEE 'script.js')
+  const actualDay = index + 1 // GET THE ACTUAL NUMBER OF THIS DAY, E.G. 1 for 'days[0]'
+  const dayString = (actualDay < 10 ? "0" + actualDay : actualDay) // IF 'actualDay' < 10, THEN ADD "0" BEFORE THE NUMBER OF THE DAY, E.G. "01"
+  const windowElement = document.getElementById(`window-day-${dayString}`) // GET THE WINDOW ELEMENT FOR THIS DAY
+  if (checkCurrentTimeAndDay(day) == 2) { //NOTE: THIS FUNCTION CAN BE FOUND IN 'script.js'
+    setWindowOpen(windowElement, dayString) // SET THE WINDOW TO BE OPEN
+  } else if (checkCurrentTimeAndDay(day) == 1) { //NOTE: THIS FUNCTION CAN BE FOUND IN 'script.js'
+      windowElement.classList.add("window-is-openable") // SET THE WINDOW TO BE OPENABLE
+      windowElement.onclick = () => { // IF AN OPENABLE WINDOW IS OPENED
+        windowElement.classList.remove("window-is-openable") // WINDOW IS NO LONGER OPENABLE
+        setWindowOpen(windowElement, dayString) // SET THE WINDOW TO BE OPEN
       }
     }
-  }
+})
