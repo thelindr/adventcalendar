4a77d02ee4efd450f5171bc7ba1849f15a3b585f
Fredrik <osterlund.f@gmail.com>
2017-11-29 16:14:32 +0100

    JS: correction; back to test day

--- code/indexpage.js
+++ code/indexpage.js
@@ -1,40 +1,40 @@
 // CHECK ALL DAYS---WILL ONLY WORK WHEN CURRENT TIME IS 1--31 DEC
-days.forEach((day, index) => {
-  const actualDay = index + 1
-  const dayString = (actualDay < 10 ? "0" + actualDay : actualDay)
-  const windowElement = document.getElementById(`window-day-${dayString}`)
-  if (checkCurrentTimeAndDay(day) == 2) {
-    windowElement.classList.add("window-is-open")
-    windowElement.onclick = () => {
-      window.location.href = `./days/${dayString}.html`
-    }
-  } else if (checkCurrentTimeAndDay(day) == 1) {
-      windowElement.classList.add("window-is-openable")
-      windowElement.onclick = () => {
-        windowElement.classList.remove("window-is-openable")
-        windowElement.classList.add("window-is-open")
-        windowElement.onclick = () => {
-          window.location.href = `./days/${dayString}.html`
-        }
-      }
-    }
-})
-
-// USE FOR TEST: APPLIED TO WINDOW FOR DAY 1
-// if (checkCurrentTimeAndDay(days[24]) == 2) {
-//   const windowElement = document.getElementById("window-day-01")
-//   windowElement.classList.add("window-is-open")
-//   windowElement.onclick = () => {
-//     window.location.href = "./days/01.html"
-//   }
-// } else if (checkCurrentTimeAndDay(days[24]) == 1) {
-//     const windowElement = document.getElementById("window-day-01")
-//     windowElement.classList.toggle("window-is-openable")
+// days.forEach((day, index) => {
+//   const actualDay = index + 1
+//   const dayString = (actualDay < 10 ? "0" + actualDay : actualDay)
+//   const windowElement = document.getElementById(`window-day-${dayString}`)
+//   if (checkCurrentTimeAndDay(day) == 2) {
+//     windowElement.classList.add("window-is-open")
 //     windowElement.onclick = () => {
-//       windowElement.classList.remove("window-is-openable")
-//       windowElement.classList.add("window-is-open")
+//       window.location.href = `./days/${dayString}.html`
+//     }
+//   } else if (checkCurrentTimeAndDay(day) == 1) {
+//       windowElement.classList.add("window-is-openable")
 //       windowElement.onclick = () => {
-//         window.location.href = "./days/01.html"
+//         windowElement.classList.remove("window-is-openable")
+//         windowElement.classList.add("window-is-open")
+//         windowElement.onclick = () => {
+//           window.location.href = `./days/${dayString}.html`
+//         }
 //       }
 //     }
-//   }
+// })
+
+// USE FOR TEST: APPLIED TO WINDOW FOR DAY 1
+if (checkCurrentTimeAndDay(days[24]) == 2) {
+  const windowElement = document.getElementById("window-day-01")
+  windowElement.classList.add("window-is-open")
+  windowElement.onclick = () => {
+    window.location.href = "./days/01.html"
+  }
+} else if (checkCurrentTimeAndDay(days[24]) == 1) {
+    const windowElement = document.getElementById("window-day-01")
+    windowElement.classList.toggle("window-is-openable")
+    windowElement.onclick = () => {
+      windowElement.classList.remove("window-is-openable")
+      windowElement.classList.add("window-is-open")
+      windowElement.onclick = () => {
+        window.location.href = "./days/01.html"
+      }
+    }
+  }
